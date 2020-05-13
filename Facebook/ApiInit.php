984c36ed074b0df222722da55a4ee9a193981096
Dominik Kasprzak <dominik.kasprzak@progrupa.com>
2020-05-13 12:50:31 +0200

    Disable CrashReporter to stop annoying everyone on env with no configured facebook credentials

--- Facebook/ApiInit.php
+++ Facebook/ApiInit.php
@@ -4,12 +4,13 @@ namespace Progrupa\FacebookAudienceBundle\Facebook;
 
 
 use FacebookAds\Api;
+use FacebookAds\CrashReporter;
 
 class ApiInit
 {
     public function __construct($facebookClientId, $facebookClientSecret, $facebookMarketingToken)
     {
-        Api::init($facebookClientId, $facebookClientSecret, $facebookMarketingToken);
+        Api::init($facebookClientId, $facebookClientSecret, $facebookMarketingToken, false);
     }
 
 }
